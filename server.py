4578656aa7a2d7d4b61db4efe7a30d2b32dce8d7
Mika Patterson <mknpatt21@gmail.com>
2021-09-29 19:55:21 +0000

    Completed movie ratings lab part 1

---
+++ server.py
@@ -0,0 +1,24 @@
+"""Server for movie ratings app."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Replace this with routes and view functions!
+
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0", debug=True)
+
+
+
+# Column name     Notes
+
+# user_id      integer, autoincrementing primary key
+
+# email         string, no char limit, unique
+
+# password      string, no char limit
